4fe58e05b7c9035a758ffd2b64de1ac468bece28
OussamaBouguerra03 <122405730+OussamaBouguerra03@users.noreply.github.com>
2024-09-21 18:23:33 +0100

    Update GenerateSecretKey.java

--- dev/src/main/java/com/bouguerra/dev/config/GenerateSecretKey.java
+++ dev/src/main/java/com/bouguerra/dev/config/GenerateSecretKey.java
@@ -7,7 +7,7 @@ import java.util.Base64;
 public class GenerateSecretKey {
     public static void main(String[] args) throws Exception {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
-        keyGenerator.init(256); // Sélectionnez la taille de la clé
+        keyGenerator.init(256);  
         SecretKey secretKey = keyGenerator.generateKey();
         String base64Key = Base64.getEncoder().encodeToString(secretKey.getEncoded());
         System.out.println("Generated Secret Key: " + base64Key);
