4b8b2891985909a29021b40b66f3ba9d64e0b9b1
OussamaBouguerra03 <122405730+OussamaBouguerra03@users.noreply.github.com>
2024-09-21 18:17:50 +0100

    Update ProjectService.java

--- dev/src/main/java/com/bouguerra/dev/services/ProjectService.java
+++ dev/src/main/java/com/bouguerra/dev/services/ProjectService.java
@@ -7,23 +7,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ProjectService {
      ProjectDTO createProject(ProjectDTO projectDTO, MultipartFile videoFile) throws Exception;
-      // Récupérer tous les projets
-    List<ProjectDTO> getAllProjects();
+     List<ProjectDTO> getAllProjects();
 
-    // Récupérer un projet par ID
-    ProjectDTO getProjectById(Long id);
+     ProjectDTO getProjectById(Long id);
 
-    // Récupérer tous les projets d'un utilisateur par ID
-    List<ProjectDTO> getProjectsByUserId(Long userId);
+     List<ProjectDTO> getProjectsByUserId(Long userId);
 
-    // Mettre à jour un projet existant
-    void updateProject(Long id, ProjectDTO projectDTO, MultipartFile videoFile) throws Exception;
+     void updateProject(Long id, ProjectDTO projectDTO, MultipartFile videoFile) throws Exception;
 
-    // Supprimer un projet par ID
-    void deleteProject(Long id);
+     void deleteProject(Long id);
 
-    // Récupérer la vidéo d'un projet par ID
-    byte[] getProjectVideo(Long id);
+     byte[] getProjectVideo(Long id);
     String getProjectNameById(Long id);
 
 }
